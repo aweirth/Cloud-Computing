c037cdded5b68e10671c79a2033feeebc68a9402
Alex Weirth <123027477+aweirth@users.noreply.github.com>
2024-06-26 21:52:26 -0700

    Update dataframe.py

--- apachespark/dataframe.py
+++ apachespark/dataframe.py
@@ -1,59 +1,37 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# dataframe.py - Spark Python example for DataFrame operations
 
-# To run this example use
-# ./bin/spark-submit examples/src/main/r/dataframe.R
-
-library(SparkR)
+from pyspark.sql import SparkSession
 
 # Initialize SparkSession
-sparkR.session(appName = "SparkR-DataFrame-example")
+spark = SparkSession.builder.appName("Spark DataFrame Example").getOrCreate()
 
-# Create a simple local data.frame
-localDF <- data.frame(name=c("John", "Smith", "Sarah"), age=c(19, 23, 18))
-
-# Convert local data frame to a SparkDataFrame
-df <- createDataFrame(localDF)
+# Create a simple local DataFrame
+data = [("John", 19), ("Smith", 23), ("Sarah", 18)]
+columns = ["name", "age"]
+localDF = spark.createDataFrame(data, columns)
 
 # Print its schema
-printSchema(df)
-# root
-#  |-- name: string (nullable = true)
-#  |-- age: double (nullable = true)
+localDF.printSchema()
 
 # Create a DataFrame from a JSON file
-path <- file.path(Sys.getenv("SPARK_HOME"), "examples/src/main/resources/people.json")
-peopleDF <- read.json(path)
-printSchema(peopleDF)
-# root
-#  |-- age: long (nullable = true)
-#  |-- name: string (nullable = true)
+path = "{}/examples/src/main/resources/people.json".format(spark.sparkContext.getConf().get("spark.home"))
+peopleDF = spark.read.json(path)
 
-# Register this DataFrame as a table.
-createOrReplaceTempView(peopleDF, "people")
+# Print its schema
+peopleDF.printSchema()
 
-# SQL statements can be run by using the sql methods
-teenagers <- sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+# Register this DataFrame as a table
+peopleDF.createOrReplaceTempView("people")
 
-# Call collect to get a local data.frame
-teenagersLocalDF <- collect(teenagers)
+# SQL statements can be run by using the sql method
+teenagers = spark.sql("SELECT name FROM people WHERE age >= 13 AND age <= 19")
+
+# Call collect to get a local DataFrame
+teenagersLocalDF = teenagers.collect()
 
 # Print the teenagers in our dataset
-print(teenagersLocalDF)
+for row in teenagersLocalDF:
+    print(row['name'])
 
-# Stop the SparkSession now
-sparkR.session.stop()
+# Stop the SparkSession
+spark.stop()
